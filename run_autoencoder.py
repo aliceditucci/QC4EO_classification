a6c42002c4270c1185714c7c889a73258e7ad003
alessandrosebastianelli <alessandroseba1995@gmail.com>
2025-05-28 13:22:33 +0000

    small refactoring

--- run_autoencoder.py
+++ run_autoencoder.py
@@ -16,22 +16,18 @@ if __name__ == "__main__":
     #region input arguments
     # Parse the arguments from the command line
     parser = argparse.ArgumentParser()
-    parser.add_argument("--N", help="Number of qubits", required=False, type=int, default=12)
-    parser.add_argument("--numbands", help="Number of bands", required=False, type=int, default=3)
-    parser.add_argument("--epochs", help="Number of epochs", required=False, type=int, default=5)
-    parser.add_argument("--perc", help="Percentage of dataset", required=False, type=float, default=0.3)
-    parser.add_argument("--method", help="Type of autoencoder: 'small' or 'sscnet' ", required=False, type=str, default='small', choices=['small', 'sscnet'])
-    parser.add_argument("--numclasses", help="Num of classes: 2 or all", required=False, type=int, default=2)
-    parser.add_argument("--batchsize", help="Batchsize", required=False, type=int, default=16)
-    parser.add_argument("--learning_rate", help="Learning rate", required=False, type=float, default=0.001)
-    parser.add_argument("--maxp", help="Percentile max", required=False, type=float, default=99.0)
-    parser.add_argument("--minp", help="Percentile min", required=False, type=float, default=0.1)
+    parser.add_argument("--N",             help="Number of qubits", required=False, type=int, default=12)
+    parser.add_argument("--numbands",      help="Number of bands", required=False, type=int, default=3)
+    parser.add_argument("--epochs",        help="Number of epochs", required=False, type=int, default=5)
+    parser.add_argument("--perc",          help="Percentage of dataset", required=False, type=float, default=0.3)
+    parser.add_argument("--method",        help="Type of autoencoder: 'small' or 'sscnet' ", required=False, type=str, default='small', choices=['small', 'sscnet', 'unet'])
+    parser.add_argument("--numclasses",    help="Num of classes: 2 or all", required=False, type=int, default=2)
+    parser.add_argument("--batchsize",     help="Batchsize", required=False, type=int, default=32)
 
 
     args = parser.parse_args()
 
-    num_qubits = args.N
-    
+    num_qubits = args.N    
     num_bands = args.numbands
 
     if num_bands == 3: bands = [3,2,1] # [3,2,1]
@@ -42,27 +38,20 @@ if __name__ == "__main__":
     method = args.method
 
     num_classes = args.numclasses   #NON SERVE PER ORA
-    if num_classes == 2:
-        classes = ['River', 'SeaLake']
-    else:
-        classes = ['AnnualCrop', 'Forest', 'HerbaceousVegetation', 'Highway', 'Industrial', 'Pasture', 'PermanentCrop', 'Residential', 'River', 'SeaLake']
+    if num_classes == 2: classes = ['River', 'SeaLake']
+    else: classes = ['AnnualCrop', 'Forest', 'HerbaceousVegetation', 'Highway', 'Industrial', 'Pasture', 'PermanentCrop', 'Residential', 'River', 'SeaLake']
         
-    batch_size = args.batchsize
-    learning_rate = args.learning_rate
-    maxp = args.maxp
-    minp = args.minp
+    batch_size    = args.batchsize
 
     print("\n\n#############################################################################")
 
-    print("\nn_qubits: ", num_qubits)
-    print("num bands: ", num_bands, 'bands: ', bands)
-    print("epochs: ", num_epochs)
-    print("percentage: ", percentage_images)
-    print("method: ", method)
-    print('classes: ', classes)
-    print('batchsize: ', batch_size)
-    print('learning rate: ', learning_rate)
-    print('maxp: ', maxp, 'minp: ', minp)
+    print("\nn_qubits: ",    num_qubits)
+    print("num bands: ",     num_bands, 'bands: ', bands)
+    print("epochs: ",        num_epochs)
+    print("percentage: ",    percentage_images)
+    print("method: ",        method)
+    print('classes: ',       classes)
+    print('batchsize: ',     batch_size)
     #endregion
 
     # Train Model
@@ -71,30 +60,33 @@ if __name__ == "__main__":
     # Instantiate LightningModule and DataModule
 
     #Initialize model
-    if method == 'small': network  = Autoencoder_small(num_bands, num_qubits)
+    if method == 'small':  network = Autoencoder_small (num_bands, num_qubits)
     if method == 'sscnet': network = Autoencoder_sscnet(num_bands, num_qubits)
+    if method == 'unet':   network = UNet(num_bands, num_qubits)
 
     data_module = EuroSATDataModule(
         whitelist_classes = classes,
         batch_size        = batch_size, 
         bands             = bands,
         mode              = 'aec',
-        num_workers       = 12,
+        num_workers       = 4,
     )
 
-    tb_logger   = pl.loggers.TensorBoardLogger(os.path.join('lightning_logs','classifiers'), name=method)
+    log_name = f"{method}_q{num_qubits}_b{num_bands}"
+
+    tb_logger   = pl.loggers.TensorBoardLogger(os.path.join('lightning_logs','classifiers'), name=log_name)
 
     # Instantiate ModelCheckpoint callback
     checkpoint_callback = ModelCheckpoint(
         dirpath=os.path.join('saved_models','classifiers'),
-        filename=method,
+        filename=log_name,
         monitor='valid_loss',
         save_top_k=1,
         mode='min',
     )
 
     # Instantiate Trainer
-    trainer = pl.Trainer(max_epochs=20, callbacks=[checkpoint_callback], logger=tb_logger)
+    trainer = pl.Trainer(max_epochs=num_epochs, callbacks=[checkpoint_callback], logger=tb_logger)
 
     # Train the model
     trainer.fit(network, data_module)
